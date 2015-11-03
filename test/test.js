2a34a72ceeee927d69e33aeca72224005ac8096c
nick hamill <nick@127tech.net>
2015-11-02 18:16:05 -0700

    tests added for identifying stream info and transcoding tests with streams

--- test/test.js
+++ test/test.js
@@ -1,18 +1,20 @@
 var sox = require('../')
+	, stream = require('stream')
   , fs = require('fs')
   , assert = require('assert')
   , path = require('path')
   , mkdirp = require('mkdirp')
   , rimraf = require('rimraf')
   , soundWav = path.join(__dirname, 'sound.wav')
-  , soundWavStream = fs.createReadStream(path.join(__dirname, 'sound.wav'), { flags: 'r'})
   , soundMp3 = path.join(__dirname, 'sound.mp3')
   , tmpDir = path.join(__dirname, 'tmp')
+  , outputWav = path.join(tmpDir, 'output.wav')
   , outputMp3 = path.join(tmpDir, 'output.mp3')
 
 describe("sox", function () {
   describe("identify", function () {
     it("stream", function(done) {
+			var soundWavStream = fs.createReadStream(path.join(__dirname, 'sound.wav'), { flags: 'r'})
       sox.identify(soundWavStream, function (err, results) {
         if (err) return done(err);
         assert.deepEqual(results, {
@@ -58,6 +60,156 @@ describe("sox", function () {
   describe("transcode", function() {
     it("creating test directory", function(done) {
       mkdirp(tmpDir, done);
+    });
+    it("stream(wav) -> stream(wav)", function(done) {
+			var write_stream = new stream.PassThrough;
+			var soundWavStream = fs.createReadStream(path.join(__dirname, 'sound.wav'), { flags: 'r'})
+      var transcode = sox.transcode(soundWavStream, write_stream, {format: 'wav'});
+      transcode.on('error', function(err) {
+        console.dir(err);
+        done(err);
+      });
+      var progress = 0;
+      var progressEventCount = 0;
+      transcode.on('progress', function(amountDone, amountTotal) {
+        var newProgress = amountDone / amountTotal;
+        progressEventCount += 1;
+        assert(newProgress >= progress);
+        progress = newProgress;
+      });
+      var gotSrc = false;
+      transcode.on('src', function(info) {
+        gotSrc = true;
+        assert.deepEqual(info, {
+          format: 'wav',
+          duration: 1.5,
+          sampleCount: 66150,
+          channelCount: 1,
+          bitRate: 0,
+          sampleRate: 44100,
+        });
+      });
+      var gotDest = false;
+      transcode.on('dest', function(info) {
+        gotDest = true;
+        assert.deepEqual(info, {
+          sampleRate: 44100,
+          format: 'wav',
+          channelCount: 2,
+          sampleCount: 66150,
+          duration: 1.5,
+          bitRate: 0,
+        });
+      });
+      transcode.on('end', function() {
+        assert(gotSrc);
+        assert(gotDest);
+        assert.strictEqual(progress, 1);
+        assert(progressEventCount >= 3, "expected at lesat 3 progress events. got: " + progressEventCount);
+        done();
+      });
+      transcode.start();
+    });
+    it("file(wav) -> stream(wav)", function(done) {
+			var write_stream = new stream.Writable;
+			write_stream.buffer = new Buffer(0);
+			write_stream._write = function(chunk, enc, next) {
+				this.buffer = Buffer.concat([this.buffer, chunk]);
+				next();
+			};
+      var transcode = sox.transcode(path.join(__dirname, 'sound.wav'), write_stream, {format: 'wav'});
+      transcode.on('error', function(err) {
+        console.dir(err);
+        done(err);
+      });
+      var progress = 0;
+      var progressEventCount = 0;
+      transcode.on('progress', function(amountDone, amountTotal) {
+        var newProgress = amountDone / amountTotal;
+        progressEventCount += 1;
+        assert(newProgress >= progress);
+        progress = newProgress;
+      });
+      var gotSrc = false;
+      transcode.on('src', function(info) {
+        gotSrc = true;
+        assert.deepEqual(info, {
+          format: 'wav',
+          duration: 1.5,
+          sampleCount: 66150,
+          channelCount: 1,
+          bitRate: 722944,
+          sampleRate: 44100,
+        });
+      });
+      var gotDest = false;
+      transcode.on('dest', function(info) {
+        gotDest = true;
+        assert.deepEqual(info, {
+          sampleRate: 44100,
+          format: 'wav',
+          channelCount: 2,
+          sampleCount: 66150,
+          duration: 1.5,
+          bitRate: 0,
+        });
+      });
+      transcode.on('end', function() {
+        assert(gotSrc);
+        assert(gotDest);
+        assert.strictEqual(progress, 1);
+        assert(progressEventCount >= 3, "expected at lesat 3 progress events. got: " + progressEventCount);
+        done();
+      });
+      transcode.start();
+    });
+    it("stream(wav) -> file(wav)", function(done) {
+			var soundWavStream = fs.createReadStream(path.join(__dirname, 'sound.wav'), { flags: 'r'})
+      var transcode = sox.transcode(soundWavStream, outputWav, {format: 'wav'});
+      transcode.on('error', function(err) {
+        console.dir(err);
+        done(err);
+      });
+      var progress = 0;
+      var progressEventCount = 0;
+      transcode.on('progress', function(amountDone, amountTotal) {
+        var newProgress = amountDone / amountTotal;
+        progressEventCount += 1;
+        assert(newProgress >= progress);
+        progress = newProgress;
+      });
+      var gotSrc = false;
+      transcode.on('src', function(info) {
+        gotSrc = true;
+        assert.deepEqual(info, {
+          format: 'wav',
+          duration: 1.5,
+          sampleCount: 66150,
+          channelCount: 1,
+          bitRate: 0,
+          sampleRate: 44100,
+        });
+      });
+      var gotDest = false;
+      transcode.on('dest', function(info) {
+        gotDest = true;
+        assert.deepEqual(info, {
+          sampleRate: 44100,
+          format: 'wav',
+          channelCount: 2,
+          sampleCount: 66150,
+          duration: 1.5,
+          bitRate: 1048576,
+        });
+      });
+      transcode.on('end', function() {
+        assert(gotSrc);
+        assert(gotDest);
+        assert.strictEqual(progress, 1);
+        assert(progressEventCount >= 3, "expected at lesat 3 progress events. got: " + progressEventCount);
+        done();
+      });
+      transcode.start();
     });
     it("wav -> mp3", function(done) {
       var transcode = sox.transcode(soundWav, outputMp3);
